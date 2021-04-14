8ae58ebfd139a44b6e1a8dea28c9d2520c4c3815
woengus <olivierbiard@woengus.com>
2021-04-14 11:47:55 +0200

    placeholder scss style, img for my projects, projectsData.js

---
+++ src/data/projectsData.js
@@ -0,0 +1,42 @@
+export const projectsData = [
+  {
+    id: 1,
+    title: "Pays du monde",
+    date: "Mars 2021",
+    languages: ["React", "Javascript", "Scss"],
+    infos:
+      "Une application pour afficher des pays et leurs informations",
+    img: "./assets/img/projet-1.jpg",
+    link: "http://www.google.com",
+  },
+  {
+    id: 2,
+    title: "Pokédex",
+    date: "Janvier 2021",
+    languages: ["Node-js", "Javascript"],
+    infos:
+      "Une application pour afficher des pokémon",
+    img: "./assets/img/projet-2.jpg",
+    link: "http://www.google.com",
+  },
+  {
+    id: 3,
+    title: "Liste de jeu",
+    date: "Avril 2021",
+    languages: ["React", "Javascript", "Node-js"],
+    infos:
+      "Une appli simple pour intéragir avec une base de donnée",
+    img: "./assets/img/projet-3.jpg",
+    link: "http://www.google.com",
+  },
+  {
+    id: 4,
+    title: "RogueLite",
+    date: "2021",
+    languages: ["Unity", "C#"],
+    infos:
+      "Mon projet perso de jeu rétro, de type roguelite, fortement inspiré de The Dinding of Isaac",
+    img: "./assets/img/projet-4.jpg",
+    link: "http://www.google.com",
+  },
+];
