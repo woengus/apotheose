c753ab2806b8b155b202742037b1bb4d4d1dd390
woengus <olivierbiard@woengus.com>
2021-04-14 20:59:25 +0200

    route correction, with all projects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { } from 'react-dom';
-import { Route, Switch } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Acceuil from './pages/Acceuil';
 import Apropos from './pages/Apropos';
 import Competences from './pages/Competences';
 import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
+import { Project1, Project2, Project3, Project4 } from './pages/Projects';
 
 const App = () => {
   return (
     <div>
      <Switch>
       <Route exact path="/" component={Acceuil} />
-      <Route exact path="/Portfolio" component={Portfolio} />
-      <Route exact path="/A propos" component={Apropos} />
-      <Route exact path="/Compétences" component={Competences} />
-      <Route exact path="/Contact" component={Contact} />
-
-      
+      <Route exact path="/projet1" component={Project1} />
+      <Route exact path="/projet2" component={Project2} />
+      <Route exact path="/projet3" component={Project3} />
+      <Route exact path="/projet4" component={Project4} />
+      <Route exact path="/a propos" component={Apropos} />
+      <Route exact path="/compétences" component={Competences} />
+      <Route exact path="/contact" component={Contact} />
+      <Redirect to="/" />
      </Switch>
     
 
