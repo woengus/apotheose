de1cf4af1ad9cae4f61040bce75b8b479946e8cb
woengus <olivierbiard@woengus.com>
2021-04-19 13:25:59 +0200

    random circle on Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 import { projectsData } from '../data/projectsData';
-import { NavLink} from 'react-router-dom'
+
 //composant pour afficher le projet courant, avec son nom, date, langages puis les infos/titre au hover sur l'image, et bouton cliquable sous l'image
 const Project = (props) => {
     const [currentProject] = useState(projectsData);
     const project = currentProject[props.projectNumber];
+
+    //random du cercle de background
+    let left = Math.floor(Math.random() * 200 + 1000) + "px"; //le +1000 assure que le cercle apparait à droite de l'écran
+    let top = Math.floor(Math.random() * 200 + 150) + "px"; //le +150 assure que le cercle se retrovuera en bas de l'écran
+    let size = "scale(" + (Math.random() +0.7) + ")";
+
     return (
         <div className="project-main">
             <div className="project-content">
@@ -30,6 +36,7 @@ const Project = (props) => {
                          </a>
                 </div>    
             </div>
+            <span className="random-circle" style ={{left:left, top:top, transform: size}}></span>
         </div>
     );
 };
